803880f1c204fad9cf5a9e65ca1eb72f8fb64071
LorenzoMarnat <lmarnat14@gmail.com>
2020-10-20 10:17:48 +0200

    Applying gravity

---
+++ Fluid/Assets/Particule.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particule : MonoBehaviour
+{
+    public float mass;
+
+    private float gravity = -9.8f;
+
+    public Vector3 acceleration;
+    public Vector3 speed;
+
+    //private float Cd = 0.25f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mass = Random.Range(1, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewAcceleration();
+        NewSpeed();
+        NewPosition();
+    }
+
+    private void NewAcceleration()
+    {
+        acceleration = new Vector3(0, gravity, 0);
+        acceleration -= speed * speed.magnitude;
+        acceleration /= mass;
+    }
+    private void NewSpeed()
+    {
+        speed += acceleration * Time.deltaTime;
+    }
+    private void NewPosition()
+    {
+        transform.position += speed * Time.deltaTime;
+    }
+}
---
+++ Fluid/Assets/Spawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int count = 1000;
+    public GameObject particule;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0;i <count;i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10));
+            Instantiate(particule, pos, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
